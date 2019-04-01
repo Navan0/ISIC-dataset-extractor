fa6e8af1fe6aec49f689e0e0a334622cf4f1d8cf
Navaneeth anand <nktclt@gmail.com>
2019-04-01 18:23:05 +0530

    update dataset-preprocessor

--- data_preprocessor.py
+++ data_preprocessor.py
@@ -1,11 +1,11 @@
 import json
 import os
 from pprint import pprint
-path, dirs, files = next(os.walk("/home/navaneeth/work/snapuskin/mainset"))
+path, dirs, files = next(os.walk(path-to-dataset"))
 file_count = int(len(files)/2)
 for i in range(file_count):
     print(str(i))
-    with open('/home/navaneeth/work/snapuskin/mainset/ISIC_0000'+str('%03d' % i)+'.json') as json_file:
+    with open('path-to-dataset/ISIC_0000'+str('%03d' % i)+'.json') as json_file:
         data = json.load(json_file)
     name = str(data['meta']['clinical']['diagnosis'])
-    os.rename('/home/navaneeth/work/snapuskin/mainset/ISIC_0000'+str('%03d' % i)+'.jpg','/home/navaneeth/work/snapuskin/renamed/'+str(name)+str(i)+'.jpg')
+    os.rename('path-to-dataset/ISIC_0000'+str('%03d' % i)+'.jpg','path-to-save-directory/'+str(name)+str(i)+'.jpg')
